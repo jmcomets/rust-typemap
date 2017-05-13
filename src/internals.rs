765636504d4312bfb40089cd88ba3fd928950365
Jean-Marie Comets <jean.marie.comets@gmail.com>
2017-05-13 17:12:37 +0200

    Use macros to reduce code duplication

--- src/internals.rs
+++ src/internals.rs
@@ -2,6 +2,49 @@ use uany::{UnsafeAny, UnsafeAnyExt};
 use std::any::Any;
 use std::fmt::Debug;
 
+/// Auto-implements `UnsafeAnyExt` for the given type, plus its Send/Sync combinations.
+#[doc(hidden)] // Not actually exported
+macro_rules! uany_ext {
+    ( $x:tt ) => {
+        unsafe impl UnsafeAnyExt for $x {}
+        unsafe impl UnsafeAnyExt for $x + Send {}
+        unsafe impl UnsafeAnyExt for $x + Sync {}
+        unsafe impl UnsafeAnyExt for $x + Send + Sync {}
+    }
+}
+
+///! Auto-implements `Implements` for the given type, plus its Send/Sync combinations.
+#[doc(hidden)] // Not actually exported
+macro_rules! implements {
+    ( $x:tt ) => {
+        unsafe impl<T: $x> Implements<$x> for T {
+            fn into_object(self) -> Box<$x> {
+                Box::new(self)
+            }
+        }
+
+        unsafe impl<T: $x + Send> Implements<($x + Send)> for T {
+            fn into_object(self) -> Box<$x + Send> {
+                Box::new(self)
+            }
+        }
+
+        unsafe impl<T: $x + Sync> Implements<($x + Sync)> for T {
+            fn into_object(self) -> Box<$x + Sync> {
+                Box::new(self)
+            }
+        }
+
+        unsafe impl<T: $x + Send + Sync> Implements<($x + Send + Sync)> for T {
+            fn into_object(self) -> Box<$x + Send + Sync> {
+                Box::new(self)
+            }
+        }
+    };
+}
+
+implements!(UnsafeAny);
+
 /// A marker trait meant for use as the `A` parameter in `TypeMap`.
 ///
 /// This can be used to construct `TypeMap`s containing only types which
@@ -13,10 +56,8 @@ use std::fmt::Debug;
 pub trait DebugAny: Any + Debug { }
 impl<T: Any + Debug> DebugAny for T { }
 
-unsafe impl UnsafeAnyExt for DebugAny {}
-unsafe impl UnsafeAnyExt for DebugAny + Send {}
-unsafe impl UnsafeAnyExt for DebugAny + Sync {}
-unsafe impl UnsafeAnyExt for DebugAny + Send + Sync {}
+implements!(DebugAny);
+uany_ext!(DebugAny);
 
 /// A marker trait meant for use as the `A` parameter in `TypeMap`.
 ///
@@ -54,6 +95,9 @@ impl<T: Any + Clone> CloneAny for T {
     }
 }
 
+implements!(CloneAny);
+uany_ext!(CloneAny);
+
 impl Clone for Box<CloneAny> {
     fn clone(&self) -> Box<CloneAny> { (**self).clone_any() }
 }
@@ -70,56 +114,7 @@ impl Clone for Box<CloneAny + Send + Sync> {
     fn clone(&self) -> Box<CloneAny + Send + Sync> { (**self).clone_any_send_sync() }
 }
 
-unsafe impl UnsafeAnyExt for CloneAny {}
-unsafe impl UnsafeAnyExt for CloneAny + Send {}
-unsafe impl UnsafeAnyExt for CloneAny + Sync {}
-unsafe impl UnsafeAnyExt for CloneAny + Send + Sync {}
-
 #[doc(hidden)] // Not actually exported
 pub unsafe trait Implements<A: ?Sized + UnsafeAnyExt> {
     fn into_object(self) -> Box<A>;
-}
-
-unsafe impl<T: UnsafeAny> Implements<UnsafeAny> for T {
-    fn into_object(self) -> Box<UnsafeAny> { Box::new(self) }
-}
-
-unsafe impl<T: UnsafeAny + Send> Implements<(UnsafeAny + Send)> for T {
-    fn into_object(self) -> Box<UnsafeAny + Send> { Box::new(self) }
-}
-
-unsafe impl<T: UnsafeAny + Sync> Implements<(UnsafeAny + Sync)> for T {
-    fn into_object(self) -> Box<UnsafeAny + Sync> { Box::new(self) }
-}
-
-unsafe impl<T: UnsafeAny + Send + Sync> Implements<(UnsafeAny + Send + Sync)> for T {
-    fn into_object(self) -> Box<UnsafeAny + Send + Sync> { Box::new(self) }
-}
-
-unsafe impl<T: CloneAny> Implements<CloneAny> for T {
-    fn into_object(self) -> Box<CloneAny> { Box::new(self) }
-}
-
-unsafe impl<T: CloneAny + Send> Implements<(CloneAny + Send)> for T {
-    fn into_object(self) -> Box<CloneAny + Send> { Box::new(self) }
-}
-
-unsafe impl<T: CloneAny + Send + Sync> Implements<(CloneAny + Send + Sync)> for T {
-    fn into_object(self) -> Box<CloneAny + Send + Sync> { Box::new(self) }
-}
-
-unsafe impl<T: DebugAny> Implements<DebugAny> for T {
-    fn into_object(self) -> Box<DebugAny> { Box::new(self) }
-}
-
-unsafe impl<T: DebugAny + Send> Implements<DebugAny + Send> for T {
-    fn into_object(self) -> Box<DebugAny + Send> { Box::new(self) }
-}
-
-unsafe impl<T: DebugAny + Sync> Implements<DebugAny + Sync> for T {
-    fn into_object(self) -> Box<DebugAny + Sync> { Box::new(self) }
-}
-
-unsafe impl<T: DebugAny + Send + Sync> Implements<DebugAny + Send + Sync> for T {
-    fn into_object(self) -> Box<DebugAny + Send + Sync> { Box::new(self) }
 }
